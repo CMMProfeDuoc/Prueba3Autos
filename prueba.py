a477c7626a864b52a50cde947d4126624f850690
CMMProfeDuoc <ca.felipe@profesor.duoc.cl>
2023-06-22 09:58:09 -0400

    ajustes

--- prueba.py
+++ prueba.py
@@ -12,17 +12,13 @@ Cada vehiculo tiene los siguientes datos:
 
 Su programa debe mostrar un menu con las siguientes a continuación.
 
-Buscar 1 auto. << OBLIGATORIO
-    Se puede buscar un auto por su "modelo" , "marca" o "año".
+Buscar 1 auto por Modelo. << OBLIGATORIO
+    Se puede buscar un auto por su "modelo".
     Se deben mostrar todos los datos del vehiculo.
 
 Imprimir la lista
     El usuario podra escribir una llave y un parametro y
     el programa debe mostrar toda la lista de vehiculos que cumplan esa condicion
-
-Filtrar por parametros
-    Cree una funcion que permita filtrar por una llave y un valor ingresado por el usuario
-    La funcion debe devolver una sublista con los parametros filtrados
 
 Imprimir Certificado
     El programa debe preguntar por el nombre del usuario y la fecha.
@@ -33,17 +29,6 @@ Imprimir Certificado
         De color <color>
         Queda registrado oficialmente a la fecha de <fecha>
 
-Agregar vehiculo
-    El programa debe preguntar los datos del vehiculo y guardarlos.
-
-    Y luego guardar estos datos en la lista.
-    (Esto se verificara si el dato es accesible)
-
-Asignar dueño
-    Debe poder agregar la opcion de asignarle un dueño a un vehiculo
-    Para esto su programa debe añadir el dato a su diccionario (o lista)
-    como lo estime conveniente
-    
 Salir.
     El programa debe cerrarse y terminar. (se finaliza el programa)
 
@@ -51,5 +36,13 @@ Luego de realizar una accion, el programa debe volver al menu principal
 (excepto por la opcion Salir).
 """
 
+#------NO CAMBIAR ---------
 from autoHerramientas import *
+#---------------------------
+
+#puede cargar cualquier archivo cambiando este parametro
+nombre_archivo = "Autos1"
+
+lista = obtenerAutos(nombre_archivo)
+
 
